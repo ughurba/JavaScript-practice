a976d372555d53ef6d3a0f42c25b6ece10d42ebf
ughurba <ughurba@edu.code.az>
2022-05-03 01:39:33 +0400

    I solved one task

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+//Напишите функцию, которая проверяет, является ли элемент именно простым объектом, а не массивом, null и т.п.
+
+let checkObj = (obj) => typeof obj === 'object' && !Array.isArray(obj) && obj !== null
+
+console.log(checkObj({}))
 
 

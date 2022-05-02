8d2e9f20362c8ebe57488420a3e4cbb6d2d40e52
ughurba <ughurba@edu.code.az>
2022-05-03 01:49:06 +0400

    I solved two task

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 //Напишите функцию, которая проверяет, является ли элемент именно простым объектом, а не массивом, null и т.п.
 
-let checkObj = (obj) => typeof obj === 'object' && !Array.isArray(obj) && obj !== null
+// let checkObj = (obj) => typeof obj === 'object' && !Array.isArray(obj) && obj !== null
 
-console.log(checkObj({}))
+// console.log(checkObj({}))
 
 
+
+//Напишите функцию, которая возвращает новый объект без указанных значений.
+//(arrow function)
+// let newObject = (obj,key) => {
+//     delete obj[key]
+//     return obj
+// }
+// console.log(newObject({a:12,b:4},'a'));
+
+
+
+
+// function updateObj(obj,key){
+//     delete obj[key]
+//     return obj
+// }
+
+//  let newObj = updateObj({a:12,b:4},'a')
+// console.log(newObj)
